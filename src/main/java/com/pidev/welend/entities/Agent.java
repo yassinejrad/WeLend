0f8ee18a7b5b694d849331c53f69f28aead46c78
Yassine Jrad <yassine.jrad1@esprit.tn>
2023-03-10 16:39:32 +0100

    @Getter @Setter added

--- src/main/java/com/pidev/welend/entities/Agent.java
+++ src/main/java/com/pidev/welend/entities/Agent.java
@@ -1,9 +1,14 @@
 package com.pidev.welend.entities;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Date;
 
+@Getter
+@Setter
 @Entity
 @Table( name = "Agent")
 public class Agent implements Serializable {
--- src/main/java/com/pidev/welend/entities/Consultation.java
+++ src/main/java/com/pidev/welend/entities/Consultation.java
@@ -1,10 +1,15 @@
 package com.pidev.welend.entities;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Date;
 import java.util.Set;
 
+@Getter
+@Setter
 @Entity
 @Table( name = "Consultation")
 public class Consultation implements Serializable {
